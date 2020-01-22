78c52da7409a422194a22b33ffd9d8a873341f7e
Lorenzo Bonicelli <lorenzobonicelli@libero.it>
2020-01-22 22:12:12 +0100

    Add base modules for auth and users

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { KittensModule } from './kittens/kittens.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-	imports: [KittensModule],
+	imports: [KittensModule, AuthModule, UsersModule],
 	controllers: [AppController],
 	providers: [AppService],
 })
